e93a3cbca517719e735f142dc8ef88fb5bc3757c
noahperkins3 <noahperkins3@gmail.com>
2023-06-18 23:07:44 -0700

    Ready to deploy

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 export function Map(props) {
+
+    function handleClick(e) {
+        e.preventDefault();
+    }
+
+    const placesVisited = [];
+
     const mapContainerStyle = {
         height: '400px',
         width: '70%',
-        "margin-left": "auto",
-        "margin-right": "auto",
+        marginLeft: "auto",
+        marginRight: "auto",
         transform: "translateY(50%)",
-        "border-radius": "10px",
+        borderRadius: "10px",
     };
 
     const center = {
@@ -23,16 +30,25 @@ export function Map(props) {
 
     return (
         <div className="travel-container">
-        <div className="map-container">
-            <h1>Interactive Map</h1>
-        <LoadScript googleMapsApiKey="AIzaSyD8j_NkF2YhA8Aas-FFB3NEW13zVG7AdL8">
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                center={center}
-                zoom={mapOptions.zoom}
-            />
-        </LoadScript>
-        </div>
+            <div className="map-container">
+                <h1>Interactive Map</h1>
+                <LoadScript googleMapsApiKey="AIzaSyD8j_NkF2YhA8Aas-FFB3NEW13zVG7AdL8">
+                    <GoogleMap
+                        mapContainerStyle={mapContainerStyle}
+                        center={center}
+                        zoom={mapOptions.zoom}
+                    />
+                </LoadScript>
+            </div>
+            <div className="travel-checklist">
+                {/* <h1>Countries I've Visited</h1> */}
+                <ul>
+                    {placesVisited.map((place) => (
+                        <li id="location-item" onClick={handleClick}>{place}</li>
+                    ))}
+                    <li>Test</li>
+                </ul>
+            </div>
         </div>
     );
 }
